5b4336e1e9d45e64833e4cba1b2daad70886933c
Tharaka Wijebandara <tharaka.nw@gmail.com>
2016-10-22 06:47:56 +0530

    #10 Add file uploader directive  for upload data files

--- web/ui/src/app/home/home.component.ts
+++ web/ui/src/app/home/home.component.ts
@@ -1,11 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Directive } from '@angular/core';
+import { FileUploader } from 'ng2-file-upload';
+
+const URL = 'http://localhost:3000/upload';
 
 @Component({
   selector: 'sc-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
+
 export class HomeComponent implements OnInit {
+
+  public uploader:FileUploader = new FileUploader({url: URL});
+  public hasBaseDropZoneOver:boolean = false;
+  public hasAnotherDropZoneOver:boolean = false;
+
+  public fileOverBase(e:any):void {
+    this.hasBaseDropZoneOver = e;
+  }
 
   constructor() {
     // Do stuff
